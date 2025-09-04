159e1c63e0943d6b3a812e0a5ef80c1bdc5a788f
一品玩家 <firstplayer1208@gmail.com>
2025-09-04 17:19:01 +0800

    初始化一个Monorepo结构，新加了技术栈.md

---
+++ packages/config/eslint/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: false,
+  extends: ['eslint:recommended'],
+  env: { es2021: true, node: true, browser: true },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
+};
+
+
